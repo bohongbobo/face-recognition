d96db8ab2d8d8d65e875fda1d344ed9e3338e57c
Bohong <bohongbobo@gmail.com>
2020-01-27 17:13:42 -0600

    update

---
+++ build/precache-manifest.24e6411489ce31af0bc49f9848dd5358.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13b0b258516a5fc73a461ce53ae4ae40",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33aaf9f931a786e66326",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "8f2a11712495bd200547",
+    "url": "/static/css/main.d79770c8.chunk.css"
+  },
+  {
+    "revision": "33aaf9f931a786e66326",
+    "url": "/static/js/2.86100a16.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.86100a16.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8f2a11712495bd200547",
+    "url": "/static/js/main.125ffdb8.chunk.js"
+  },
+  {
+    "revision": "b3750fd1a40b1dc88331",
+    "url": "/static/js/runtime-main.ebf00c01.js"
+  }
+]);
