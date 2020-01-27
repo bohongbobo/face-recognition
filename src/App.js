adb5e6f946e7a6a9dce5f18b3492efc0398df3a4
Bohong <bohongbobo@gmail.com>
2020-01-27 17:57:59 -0600

    update database

--- src/App.js
+++ src/App.js
@@ -86,7 +86,7 @@ displayFaceBox = (box) => {
 
   onButtonSubmit = () => {
     this.setState({imgUrl: this.state.input});
-    fetch('http://localhost:3000/imageurl', {
+    fetch('https://warm-shore-01600.herokuapp.com/imageurl', {
       method: 'post',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
@@ -96,7 +96,7 @@ displayFaceBox = (box) => {
       .then(response => response.json())
       .then(response => {
         if(response){
-          fetch('http://localhost:3000/image', {
+          fetch('https://warm-shore-01600.herokuapp.com/image', {
             method: 'put',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
--- src/componments/Signin/Signin.js
+++ src/componments/Signin/Signin.js
@@ -16,7 +16,7 @@ class Signin extends Component {
     }
 
     onSubmitSignIn = () => {
-        fetch('http://localhost:3000/signin', {
+        fetch('https://warm-shore-01600.herokuapp.com/signin', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
